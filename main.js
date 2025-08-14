671a942289d86f6acde0c0a85a4b20d29dbdca1d
Li-Mateus <mateushardware@hotmail.com>
2025-08-14 15:17:23 -0300

    Adicionando exceções/ tratativas de erro

--- main.js
+++ main.js
@@ -7,16 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const followingElement = document.querySelector('.seguindo');
     const linkElement = document.querySelector('.profile-link');
 
-    fetch('https://api.github.com/users/Li-Mateus').then(function(res) {
+    fetch('https://api.github.com/users/Li-Mateus')
+    .then(function(res) {
+        if (!res.ok) {
+            throw new Error(`Erro HTTP: ${res.stauts}`);
+        }
         return res.json();
     })
     .then(function(json) {
         avatarElement.src = json.avatar_url;
-        nameElement.innerText = json.name;
+        nameElement.innerText = json.name || "Nome não encontrado";
         userElement.innerText = '@' + json.login;
         reposElement.innerText = json.public_repos;
         followersElement.innerText = json.followers;
         followingElement.innerText = json.following;
         linkElement.href = json.html_url;
+    })
+    .catch(function(error) {
+        console.error("Ocorreu um erro:", error);
+        nameElement.innerText = "Erro ao carregar dados";
     })
 })
