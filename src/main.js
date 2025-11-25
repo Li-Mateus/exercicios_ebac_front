bef2228c063c571e4e704201dcb779695ccefef8
Li-Mateus <mateushardware@hotmail.com>
2025-11-25 18:19:14 -0300

    Commit inicial

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicaNumeros(a, b) {
+    return a * b;
+}
+// exemplo para a função acima:
+console.log(multiplicaNumeros(3, 5));
+function saudacoes(nome) {
+    return `Olá ${nome} :)`;
+}
+// exemplo para a segunda função:
+console.log(saudacoes('Mateus'));
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+function multiplicaNumeros(a: number, b: number): number {
+    return a * b;
+}
+
+// exemplo para a função acima:
+console.log(multiplicaNumeros(3, 5));
+
+function saudacoes(nome: string) {
+    return `Olá ${nome} :)`;
+}
+
+// exemplo para a segunda função:
+console.log(saudacoes('Mateus'));
